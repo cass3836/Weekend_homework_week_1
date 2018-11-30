07c16a97f548f579de3f7d379151b58593f0c407
cass3836 <cassia@gmx.co.uk>
2018-11-30 19:14:11 +0000

    week 1 homework - 13 tests passed

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -40,12 +40,49 @@ def pets_by_breed(pet_shop, breed)
   return breed_found
 end
 
+
+#Both the below work, but only if there is only one pet called the given name.
+#Can't work out how to continue looping even if given name if found.
+
+# def find_pet_by_name(pet_shop, pet_name)
+#   name_found = Array.new
+#   for pet in pet_shop[:pets]
+#     if pet[:name] == pet_name
+#       name_found.push(pet)
+#     end
+#   end
+#   return name_found[0]
+# end
+
+
 def find_pet_by_name(pet_shop, pet_name)
-  name_found = Array.new
   for pet in pet_shop[:pets]
     if pet[:name] == pet_name
-      name_found.push(pet)
+      return pet
     end
   end
-  return name_found[0]
+  return nil
 end
+
+def remove_pet_by_name(pet_shop, pet_name)
+  for pet in pet_shop[:pets]
+    if pet[:name] == pet_name
+      pet_shop[:pets].delete(pet)
+    end
+  end
+end
+
+def stock_count(pet_shop)
+  return pet_shop[:pets].length
+end
+
+def add_pet_to_stock(pet_shop, new_pet)
+  pet_shop[:pets].push(new_pet)
+end
+
+#
+# def test_add_pet_to_stock
+#   add_pet_to_stock(@pet_shop, @new_pet)
+#   count = stock_count(@pet_shop)
+#   assert_equal(7, count)
+# end
