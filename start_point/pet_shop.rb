a203cf55c69a607de3793e085905f21b1a3423af
cass3836 <cassia@gmx.co.uk>
2018-11-30 13:52:26 +0000

    Week 1 homework - first nine tests passed

--- start_point/pet_shop.rb
+++ start_point/pet_shop.rb
@@ -6,8 +6,8 @@ def total_cash(pet_shop)
   return pet_shop[:admin][:total_cash]
 end
 
-def add_or_remove_cash(pet_shop, number)
-  pet_shop [:admin][:total_cash] += number
+def add_or_remove_cash(pet_shop, new_cash)
+  pet_shop[:admin][:total_cash] += new_cash
   return pet_shop[:admin][:total_cash]
 end
 
@@ -19,4 +19,23 @@ end
 
 def pets_sold(pet_shop)
   return pet_shop[:admin][:pets_sold]
+end
+
+def increase_pets_sold(pet_shop, new_sales)
+  pet_shop[:admin][:pets_sold] += new_sales
+  return pet_shop[:admin][:pets_sold]
+end
+
+def stock_count(pet_shop)
+  return pet_shop[:pets].length
+end
+
+def pets_by_breed(pet_shop, breed)
+  breed_found = Array.new
+  for pet in pet_shop[:pets]
+    if pet[:breed] == breed
+      breed_found.push(breed)
+    end
+  end
+  return breed_found
 end
